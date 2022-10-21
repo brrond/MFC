37b7125ec58f41fbfe0b07bf65c29067564326eb
greentech72 <danya17072@gmail.com>
2022-10-21 21:53:16 +0300

    Init flyway migration

---
+++ REST/src/main/java/ua/nix/onishchenko/mfc/RESTApplication.java
@@ -0,0 +1,13 @@
+package ua.nix.onishchenko.mfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RESTApplication.class, args);
+    }
+
+}
---
+++ REST/src/main/resources/db/migration/V1_0__create_tables.sql
@@ -0,0 +1,38 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+DROP TABLE IF EXISTS operation;
+DROP TABLE IF EXISTS operation_type;
+DROP TABLE IF EXISTS account;
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE "user" (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    name VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creation TIMESTAMP,
+
+    CONSTRAINT proper_email CHECK (email ~* '[^@\s]+@[^@\s]+\.[^@\s]+')
+);
+
+CREATE TABLE account (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    holder uuid REFERENCES "user"(id),
+    title VARCHAR(255) NOT NULL,
+    balance MONEY default 0,
+    creation TIMESTAMP
+);
+
+CREATE TABLE operation_type (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    creator uuid REFERENCES "user"(id),
+    title VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE operation (
+   id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+   account uuid REFERENCES account (id),
+   sum MONEY NOT NULL,
+   type uuid REFERENCES operation_type (id),
+   creation TIMESTAMP
+);
