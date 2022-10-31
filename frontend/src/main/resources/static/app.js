f7b9f095fbab47d36a747a2de180c83318fed556
greentech72 <danya17072@gmail.com>
2022-10-31 14:22:08 +0200

    Add page templates and static resources

---
+++ frontend/src/main/resources/static/app.js
@@ -0,0 +1,18 @@
+function loadScript(url, callback) {
+    let head = document.head;
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    head.appendChild(script);
+}
+
+loadScript("elements.js", () => {
+    initHeader();
+    initFooter();
+    initMainStyle();
+    initBackground();
+});
